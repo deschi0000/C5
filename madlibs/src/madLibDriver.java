c71ff8469b0e030d39e8ce5206958a58b3d3786b
deschi0000 <79345657+deschi0000@users.noreply.github.com>
2023-08-09 10:07:13 +0800

    use list and reduce repetition

--- madlibs/src/madLibDriver.java
+++ madlibs/src/madLibDriver.java
@@ -1,67 +1,44 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class madLibDriver {
-
-
     public static void main(String[] args) {
 
         System.out.println("Welcome to Madlibs");
-
         String[] templateArray = getTemplate();
         String[] finalMadLib = buildMadLib(templateArray);
-
-        System.out.println(String.join("Your Madlib: "));
         System.out.println(String.join(" ", finalMadLib));
-
     }
-
 
     public static String[] buildMadLib(String[] template) {
         String[] finalMadLib = new String[template.length];
         Scanner input = new Scanner(System.in);
 
+        String[] words = {"EXCLAMATION(!)", "NOUN", "ADJECTIVE", "ADVERB", "PLACE"};
+        ArrayList<String> wordTypes = new ArrayList<>(Arrays.asList(words));
+
         for (int i = 0; i < template.length; i++) {
             String response;
-            if (template[i] == "EXCL") {
-                System.out.println("Please enter an exclamation (with !): ");
+            if (wordTypes.contains(template[i])) {
+                System.out.printf("Please enter a(n) %s: ", template[i]);
                 response = input.next();
                 finalMadLib[i] = response;
-            } else if (template[i] == "NOUN") {
-                System.out.println("Please enter a noun: ");
-                response = input.next();
-                finalMadLib[i] = response;
-            } else if (template[i] == "ADJ") {
-                System.out.println("Please enter an adjective: ");
-                response = input.next();
-                finalMadLib[i] = response;
-            } else if (template[i] == "ADV") {
-                System.out.println("Please enter an adverb: ");
-                response = input.next();
-                finalMadLib[i] = response;
-            } else if (template[i] == "PLACE") {
-                System.out.println("Please enter a place: ");
-                response = input.next();
-                finalMadLib[i] = response;
-            } else {
+            }
+            else {
                 finalMadLib[i] = template[i];
             }
         }
-
         return finalMadLib;
     }
 
     public static String[] getTemplate() {
 
         Random random = new Random();
-
         String[][] templates = {
-                {"EXCL", "he said", "ADV", "as he jumped into his convertible", "NOUN", "and drove off with his", "ADJ", "wife."}
+                {"EXCLAMATION(!)", "he said", "ADVERB", "as he jumped into his convertible", "NOUN", "and drove off with his", "ADJECTIVE", "wife."}
         };
 
         int randomIndex = random.nextInt(templates.length);
 
         return templates[randomIndex];
     }
-
 }
